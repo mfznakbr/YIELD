1f14f13e80d193d1191674a614a2af98aebdf33c
mfznakbr <fauzanmhd504@gmail.com>
2025-07-12 10:56:05 +0700

    Update modeling.py

--- CI/modeling.py
+++ CI/modeling.py
@@ -10,30 +10,32 @@ from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 from preprocessing import preprocess_data
 
-
 if __name__ == "__main__":
     warnings.filterwarnings("ignore")
     np.random.seed(42)
 
     # Ambil argumen CLI
-    # Read the wine-quality csv file (make sure you're running this from the root of MLflow!)
-    file_path = sys.argv[3] if len(sys.argv) > 3 else os.path.join(os.path.dirname(os.path.abspath(__file__)), "data_train.xlxs")
-    data = pd.read_excel(file_path)
+    n_estimators = int(sys.argv[1]) if len(sys.argv) > 1 else 100
+    max_depth = int(sys.argv[2]) if len(sys.argv) > 2 else 10
+    file_path = sys.argv[3] if len(sys.argv) > 3 else os.path.join(os.path.dirname(os.path.abspath(__file__)), "data_train.xlsx")
+
+    print(f"Reading train data from: {file_path}")
+
     # Load data
-    train_df = pd.read_excel(data)
-    test_df = pd.read_excel("testing.xlsx")
+    train_df = pd.read_excel(file_path)
+    test_df = pd.read_excel("CI/testing.xlsx")  # Pastikan file ini juga tersedia saat CI/CD
+
     target = "Ton"
 
     # Preprocessing
     X_train, X_test, y_train, y_test = preprocess_data(
         train_df, test_df, target_column=target,
-        save_path="preprocessing.joblib",
-        file_path="columns.csv"
+        save_path="CI/preprocessing.joblib",
+        file_path="CI/columns.csv"
     )
 
-    # MLflow run
     with mlflow.start_run():
-        # Setup dan training model
+        # Parameter grid
         param_grid = {
             "n_estimators": [n_estimators],
             "max_depth": [max_depth],
@@ -56,24 +58,24 @@ if __name__ == "__main__":
 
         # Evaluasi
         y_pred = best_model.predict(X_test)
+
         mlflow.log_metrics({
             "test_mse": mean_squared_error(y_test, y_pred),
             "test_rmse": np.sqrt(mean_squared_error(y_test, y_pred)),
             "test_mae": mean_absolute_error(y_test, y_pred),
             "test_r2": r2_score(y_test, y_pred)
         })
+
         mlflow.log_params({
             "n_estimators": n_estimators,
             "max_depth": max_depth
         })
 
-        # Simpan model
+        # Log model
         mlflow.sklearn.log_model(
             sk_model=best_model,
             artifact_path="model",
             input_example=X_train[:1]
         )
-        accuracy = best_model.score(X_test, y_test)
-        mlflow.log_metric("accuracy", accuracy)
 
-
+        mlflow.log_metric("accuracy", best_model.score(X_test, y_test))
